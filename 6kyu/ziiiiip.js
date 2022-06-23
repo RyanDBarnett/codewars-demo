ae5ceaa25a41653012e6ce365f89e382bc43105c
RyanDBarnett <conner2468@gmail.com>
2022-06-23 09:18:52 -0600

    Add ziiiiip

---
+++ 6kyu/ziiiiip.js
@@ -0,0 +1,41 @@
+// DESCRIPTION:
+// Let's implement the zipObject function that enables such results
+//
+// zipObject(['fred', 'barney'], [30, 40])
+// => { 'fred': 30, 'barney': 40 }
+//
+// zipObject([['fred', 30], ['barney', 40]])
+// => { 'fred': 30, 'barney': 40 }
+// The zipObject creates an object composed from arrays of keys and values. It is provided with either a single two dimensional array, i.e. [[key1, value1], [key2, value2]] or with two arrays, one of keys and one of corresponding values.
+//
+// If only keys are given, then set the values to undefined.
+//
+// zipObject(['fred', 'barney'])
+// { fred: undefined, barney: undefined }
+// If neither keys nor values are specified, then return {}
+//
+// zipObject()
+// {}
+
+// My Solution
+
+function zipObject(keys, values) {
+  if (!keys) return {};
+  if (Boolean(values)) {
+    return keys.reduce((obj, key, i) => {
+      obj[key] = values[i];
+      return obj;
+    }, {});
+  } else {
+    return keys.reduce((obj, element) => {
+      var value = element[1];
+      if (Number.isInteger(value)) {
+        var key = element[0];
+        obj[key] = value;
+      } else {
+        obj[element] = undefined;
+      }
+      return obj;
+    }, {});
+  }
+}
